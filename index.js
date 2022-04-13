fe9b98c6cd40ad8b30afea6f9f3136bce2b56799
VLSLV333 <v.sherstyk@gmail.com>
2022-04-13 12:13:20 +0000

    Proxy are cool. Need alot of examples of usage, to really see their potential

--- index.js
+++ index.js
@@ -1,42 +1,42 @@
 // Wrapper
                             // буде задавати дефолтне значення ключів, якщо вони у нас не визначені
                             
-const withDefaultValue = (target, defaultValue = 0) => {
-   return new Proxy(target, {
-       get: (obj, prop) => (prop in obj) ? obj[prop] : defaultValue
-   }) 
-}
+// const withDefaultValue = (target, defaultValue = 0) => {
+//    return new Proxy(target, {
+//        get: (obj, prop) => (prop in obj) ? obj[prop] : defaultValue
+//    }) 
+// }
 
-                                    // за допомогою цього проксі ми створили новий об'єкт - position. Звертаючись до його ключів, ми отримуємо значення, які ми задали власноруч, звертаючись до будь-якого іншого ключа - отримуємо значення, яке ми задаємо defaultValue
-const position = withDefaultValue(
-    {
-    x: 24,
-    y: 42
-    },
-    33
-)
+//                                     // за допомогою цього проксі ми створили новий об'єкт - position. Звертаючись до його ключів, ми отримуємо значення, які ми задали власноруч, звертаючись до будь-якого іншого ключа - отримуємо значення, яке ми задаємо defaultValue
+// const position = withDefaultValue(
+//     {
+//     x: 24,
+//     y: 42
+//     },
+//     33
+// )
 
 // Hidden Properties     - проксі за допомогою якого ми можемо створити об'єкт до певних ключів котрого немає доступу.
 
-const withHiddenProps = (target, prefix = "_") => {
-    return new Proxy(target, {
-        has: (obj, prop) => (prop in obj) && (!prop.startsWith(prefix)),
-        ownKeys: obj => Reflect.ownKeys(obj)
-            .filter(p => !p.startsWith(prefix)),
-        get: (obj, prop, receiver) => (prop in receiver ? obj[prop] : void 0) // void 0 = код поверне undefined
-    })
-}
+// const withHiddenProps = (target, prefix = "_") => {
+//     return new Proxy(target, {
+//         has: (obj, prop) => (prop in obj) && (!prop.startsWith(prefix)),
+//         ownKeys: obj => Reflect.ownKeys(obj)
+//             .filter(p => !p.startsWith(prefix)),
+//         get: (obj, prop, receiver) => (prop in receiver ? obj[prop] : void 0) // void 0 = код поверне undefined; prop in receiver = чи залишився такий ключ в ітерації, після усіх наших фільтрацій?; obj[prop] = якщо залишився, покажіть велью за ключем
+//     })
+// }
 
-const data = withHiddenProps({
-    name: "Vladislav",
-    age: 25,
-    _userID: "100 000 IQ"
-})
+// const data = withHiddenProps({
+//     name: "Vladislav",
+//     age: 25,
+//     _userID: "100 000 IQ"
+// })
 
 // data._userID = undefined; "_userID" in data = false (думаю це has), for (let key in data) console.log(data[key]) = тільки ключі без _ доступні, Object.keys(data) = тільки ключі без _ доступні
 
 
-// Optimization
+// Optimization                                          // при додаванні нових об'єктів в наш масив ми відразу додаємо відповідний запис з потрібним айді в інший об'єкт index, для того щоб пізніше створити кастомну функцію, яка буде шукати наші об'єкти по айді
 // const userData = [
 //     {id: 1, name: "Vlad", job: "programing", age: 25},
 //     {id: 2, name: "Julia", job: "content manager", age: 20},
@@ -60,21 +60,70 @@ const data = withHiddenProps({
 // index[3]                                // тепер цей об'єкт показує відразу потрібний нам об'єкт по айді!
 
 
-const IndexedArray = new Proxy(Array, {
-    construct(target, [args]) {        // construct = робить пастку на ключове слово New; args = масив який передається в ключове слово new
-        const index = {}
-        args.forEach(item => (index[item.id] = item))
+// const IndexedArray = new Proxy(Array, {
+//     construct(target, [args]) {        // construct = робить пастку на ключове слово New; args = масив який передається в ключове слово new
+//         const index = {}
+//         args.forEach(item => (index[item.id] = item))
 
-        return new Proxy(target(...args), {
-            get(arr, prop){
-                switch (prop) {
-                    case "push": return item => {
-                        index[item.id] = item
-                        arr["push"].call(arr,item)
-                    }
-                    case "findById": return id => index[id]
-                    default: return arr[prop]
+//         return new Proxy(target(...args), {
+//             get(arr, prop){
+//                 switch (prop) {
+//                     case "push": return item => {
+//                         index[item.id] = item
+//                         arr["push"].call(arr,item)
+//                     }
+//                     case "findById": return id => index[id]
+//                     default: return arr[prop]
+//                 }
+//             }
+//         })
+//     }
+// })
+
+// const users = new IndexedArray([
+//     {id: 1, name: "Vlad", job: "programing", age: 25},
+//     {id: 2, name: "Julia", job: "content manager", age: 20},
+//     {id: 3, name: "Nastia", job: "student", age: 18},
+//     {id: 4, name: "Sasha", job: "business", age: 45}
+// ])
+
+
+
+
+// Wrapper
+
+// const withDefaultWalue = (target, defaultValue = 0) => {            // буде додавати дефолтне значення ключам, якщо вони не визначені
+//     return new Proxy (target, {
+//         get: (obj, prop) => (prop in obj ? obj[prop] : defaultValue)
+//     })
+// }
+
+// const position = withDefaultWalue ({
+//     x: 22,
+//     y: 11
+// }, 2)
+
+
+// Optimization                             
+
+const IndexedArray = new Proxy(Array, {
+    construct(target, [args]) {
+        const index = {}
+        args.forEach( person => (index[person.id] = person))
+        return new Proxy (target(...args), {
+            get (arr, prop){
+             switch(prop){
+                case "push": return item =>{
+                    index[item.id] = item
+                    arr["push"].call(arr,item)
                 }
+                case "unshift": return item =>{
+                    index[item.id] = item
+                    arr["unshift"].call(arr,item)
+                }
+                case "findById": return id => index[id]
+                 default: return arr[prop]
+             }   
             }
         })
     }
